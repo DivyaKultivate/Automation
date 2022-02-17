5d38b05ecc0234533388dd04d12e8e5c3c56253c
DivyaKultivate <90695708+DivyaKultivate@users.noreply.github.com>
2022-02-17 15:26:29 +0530

    Add files via upload

--- ChildImp.py
+++ ChildImp.py
@@ -1,14 +1,15 @@
-from Oops import Calculator
+from pythonBasics.OopsDemo import Calculator
 
 
 class ChildImpl(Calculator):
     num2 = 200
 
     def __init__(self):
-        Calculator.__init__(self, 5, 6)
+        Calculator.__init__(self, 2, 10)
 
     def getCompleteData(self):
-        return self.num2 + self.num + self.summation()
+        return self.num2 + self.num + self.Summation()
+
 
 obj = ChildImpl()
 print(obj.getCompleteData())
--- loops.py
+++ loops.py
@@ -1,133 +1,47 @@
+
 greeting = "Good Morning"
 a = 4
 
-if greeting == "Good Morning":
-    print("Condition matches")
+if a > 2:
+    print(" Condition matches")
+    print("second line")
 else:
-    print("Condition does not match")
-print("if else condition checked")
+    print("condition do not match")
 
-if greeting == "Morning":
-    print("Condition matches")
-else:
-    print("Condition does not match")
-print("if else condition checked")
+print("if else condition code is completed")
 
-if a>5:
-    print("Condition true")
-else:
-    print("Condition false")
 
-if a>=4:
-    print("Condition true")
-else:
-    print("Condition false")
+#for loop
 
-#For loop
-
-obj =1, 2, 3, 4, 5
+obj= [2, 3, 5, 7, 9]
 for i in obj:
-    print(i)
-
-obj1 =1, 2, 3, 4, 5
-for i in obj1:
     print(i*2)
 
-for j in range(1, 6):
-    print(j*5)
 
-#sum of First Natural Numbers 1+2+3+4+5
-#range(i, j) -> i to j-i
+# sum of First Natural numbers 1+2+3+4+5 = 15
+#range(i,j) -> i to j-1
 summation = 0
-for i in range(1, 6):
-    summation=summation+i
+for j in range(1, 6):
+    summation = summation + j
 print(summation)
 
-#Example1
-fruits = "Apple", "Banana", "Pomegranate"
-for x in fruits:
-    print(x)
-    if x=="Banana":
-        break
+print("*******************************")
+for k in range(1, 10, 5):
+    print(k)
+    print("**************SKIPPING FIRST INDEX*****************")
+for m in range(10):
+    print(m)
 
-#Example2
-fruits = "Apple", "Banana", "Pomegranate"
-for x in fruits:
-    if x=="Banana":
-        print(x)
-        break
 
-#Example3
-fruits = "Apple", "Banana", "Pomegranate"
-for x in fruits:
-    if x=="Banana":
-        continue
-    print(x)
 
-#Example4
-for x in range(6):
-    print(x)
 
-#Example5
-for x in range(2, 6):
-  print(x)
 
-#Example6
-for x in range(2, 30, 3):
-  print(x)
 
-#Example7
-for x in range(6):
-  print(x)
-else:
-  print("Finally finished!")
 
-#Example8
-for x in range(6):
-  if x == 3: break
-  print(x)
-else:
-  print("Finally finished!")
 
-#Example9
-adj = ["red", "big", "tasty"]
-fruits = ["apple", "banana", "cherry"]
 
-for x in adj:
-  for y in fruits:
-    print(x, y)
 
-#Example10
-for x in [0, 1, 2]:
-  pass
 
-#while
-#Example2
-it = 4
-while it>1:
-    if it != 3:
-        print(it)
-    it=it - 1
-print("while loop execution is done")
 
-#Example3
-it = 4
-while it>1:
-  if it == 3:
-    break
-  print(it)
-  it=it - 1
-print("while loop execution is done")
 
-#Example4
-it = 10
-while it>1:
-  if it == 9:
-      #it =it-1
-    continue
-  if it == 3:
-    break
-  print(it)
-  it=it - 1
-print("while loop execution is done")
 
